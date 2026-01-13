5da18c57ef7e309adf864f61fd2a5e617998972b
smatheny <smathenytech@gmail.com>
2026-01-13 15:05:05 -0600

    add vercel analytics

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Layout() {
   return (
@@ -9,6 +10,9 @@ export default function Layout() {
       <main className="mx-auto w-full max-w-6xl px-4 sm:px-6 lg:px-8">
         <Outlet />
       </main>
+      
+      <Analytics />
+
       <Footer />
     </div>
   );
